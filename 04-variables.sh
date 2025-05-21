5e6c70b480d62fc168cb4a21c5f1d1aac526d253
kolapaka <kolapaka1992@gmail.com>
2025-05-21 16:07:27 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo  "$PERSON1: hai $PERSON2 how are you?"
+echo "$PERSON2: iam good how are you $PERSON1"
+echo "$PERSON1: what are you doing $PERSON2"
+echo "$PERSON2: iam doing a project on aws"
+echo "$PERSON1: even im doing a project too"
+echo "$PERSON2: oh! great $PERSON1"
+echo "$PERSON1: thanku $PERSON2"
