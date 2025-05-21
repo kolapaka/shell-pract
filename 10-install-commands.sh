d695b10da632e86a1611876c78b12bb85e096920
kolapaka <kolapaka1992@gmail.com>
2025-05-21 23:42:06 +0530

    hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "error: : please ru this script with root access"
+exit 4 # give other than 0 upto 127
 else
 echo "you are running with root acess"
 fi
