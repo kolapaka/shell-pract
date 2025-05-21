700a1fa4b16df8ee7887dfa6453a039a8eea537e
kolapaka <kolapaka1992@gmail.com>
2025-05-21 23:01:28 +0530

    hello world started

--- 08-sp-variable.sh
+++ 08-sp-variable.sh
@@ -2,3 +2,9 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
+echo "script name: $PWD"
+echo "user running this script: $USER"
+echo "Home directory: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
