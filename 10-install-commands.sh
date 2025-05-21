9ada21be2782f77c254646d97db5a2438977eff4
kolapaka <kolapaka1992@gmail.com>
2025-05-21 23:36:00 +0530

    hello world started

---
+++ 10-install-commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [$USERID -ne 0]
+then
+echo "error: : please ru this script with root access"
+else
+echo "you are running with root acess"
+fi
+dnf install mysql -y
