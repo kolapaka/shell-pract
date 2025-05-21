27b0566647105331409a581815d55deabd3ad566
kolapaka <kolapaka1992@gmail.com>
2025-05-21 15:07:50 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Trump: "hi musk how are you"
+echo Musk: "iam good how are you Trump"
+echo Trump: "what are you doing Musk"
+echo Musk: "iam doing a project on aws"
+echo Trump: "even im doing a project too"
+echo Musk: "oh! great Trump"
+echo Trump: "thanku Musk"
