71c9e89da0edea61fc4eeda02f024c62e32181a1
kolapaka <kolapaka1992@gmail.com>
2025-05-21 19:33:48 +0530

    hello world started

--- 07-array.sh
+++ 07-array.sh
@@ -2,10 +2,11 @@
 
 
 
+
 MOVIES=("Court" "hit3" "pushpa2" "thandel")
 
-echo "First Movie: ${MOVIES[2]}"
 echo "First Movie: ${MOVIES[1]}"
+echo "First Movie: ${MOVIES[2]}"
 echo "First Movie: ${MOVIES[3]}"
 echo "First Movie: ${MOVIES[4]}"
 echo "all movies: ${MOVIES[@]}"
